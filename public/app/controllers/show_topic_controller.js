89e58fab631712986a6e9d8e62385d7b539ac7dc
opeskola <opeskola@gmail.com>
2015-05-06 00:12:35 +0300

    6.5 klo 0:12

--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,3 +1,15 @@
 FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $location, Api){
-  // Toteuta kontrolleri tähän
+    // Toteuta kontrolleri tähän
+    
+    Api.getTopics().success(function(topics){
+        $scope.topics = topics;
+    });
+    
+    Api.getTopic().success(function(id) {
+        for (var i = 0; i < $scope.topics.length; i++) {
+            if ($scope.topics[i].$routeParams.id == id) {
+                $scope.topic = $scope.topics[i];
+            }
+        }
+    });
 });
--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,29 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
+    // Toteuta kontrolleri tähän
+    
+    Api.getTopics().success(function(topics){
+        $scope.topics = topics;
+    });
+    
+    
+    $scope.addTopic = (function() {
+        if (($scope.newTopic.name != '') && ($scope.newTopic.description != '')) {
+            Api.addTopic().success(function(topic) {
+                topic.name = $scope.newTopic.name,
+                topic.description = $scope.newTopic.description
+                
+                $scope.topics.push(topic);
+                
+                $scope.newTopic.name = '';
+                $scope.newTopic.description = '';
+                
+                $location.path('/topics/:id')
+            });
+        } 
+    });
+            
+ 
+            
+       
+    
 });
