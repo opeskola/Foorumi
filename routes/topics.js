5b986aa44d5a3445ad04f44b90305ef5fb9b7851
opeskola <opeskola@gmail.com>
2015-05-05 20:59:53 +0300

    teht 50

--- routes/topics.js
+++ routes/topics.js
@@ -9,22 +9,32 @@ var Models = require('../models');
 // GET /topics
 router.get('/', function(req, res, next) {
     // Hae kaikki aihealueet tässä (Vinkki: findAll)
-    res.send(200);
+    Models.Topic.findAll().then(function(topics){
+        res.json(topics);
+    });
+    
 });
 
 // GET /topics/:id
 router.get('/:id', function(req, res, next) {
-  // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
-  var topicId = req.params.id;
-  res.send(200);
+    // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
+    var topicId = req.params.id;
+    Models.Topic.findOne(topicId).then(function(topic){
+        res.json(topic);
+    });  
 });
 
 // POST /topics
 router.post('/', function(req, res, next) {
-  // Lisää tämä aihealue
-  var topicToAdd = req.body;
-  // Palauta vastauksena lisätty aihealue
-  res.send(200);
+    // Lisää tämä aihealue
+    var topicToAdd = req.body;
+    Models.Topic.create({
+        name: topicToAdd.name,
+        description: topicToAdd.description
+    }).then(function(topic){
+        res.json([topic.name, topic.description]);
+    });
+    // Palauta vastauksena lisätty aihealue
 });
 
 // POST /topics/:id/message
