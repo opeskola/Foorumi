91694c7bf778de7b78d33ad200aeb43c29aa5e52
opeskola <opeskola@gmail.com>
2015-05-08 00:51:37 +0300

    teht 54

--- routes/messages.js
+++ routes/messages.js
@@ -12,14 +12,19 @@ router.get('/:id', function(req, res, next) {
   var messageId = req.params.id;
   Models.Message.findOne({
       where: {id: messageId},
-      include: {model: Models.Reply}
+      include: {
+          model: Models.Reply,
+          include: {
+              model: Models.User
+          }      
+      }
   }).then(function(message) {
       res.json(message);
   });
 });
 
 // POST /messages/:id/reply
-router.post('/:id/reply', function(req, res, next){
+router.post('/:id/reply', authentication, function(req, res, next){
     // Lisää tällä id:llä varustettuun viestiin...
     var messageId = req.params.id;
     // ...tämä vastaus (Vinkki: lisää ensin replyToAdd-objektiin kenttä MessageId, jonka arvo on messageId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
